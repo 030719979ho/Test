f527fbc0e4eebebc9d4add141b23083bcc47df96
Pongsakorn <swu59907@outlook.com>
2019-03-29 20:09:15 +0700

    Home Work for Cotactic

---
+++ Main/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using Main.Models;
+using Newtonsoft.Json;
+
+namespace Main.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<BookModel> people = new List<BookModel>();
+            JSONReadWrite readWrite = new JSONReadWrite();
+            people = JsonConvert.DeserializeObject<List<BookModel>>(readWrite.Read("books.json","data"));
+
+            return View(people);
+            
+        }
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+    public class JSONReadWrite
+    {
+        public JSONReadWrite() { }
+
+        public string Read(string fileName, string location)
+        {
+           
+            string root = "App_Data";
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                root,
+                location,
+                fileName);
+
+            string jsonResult;
+            
+            using (StreamReader streamReader = new StreamReader(path))
+            {
+                jsonResult = streamReader.ReadToEnd();
+            }
+            return jsonResult;
+        }
+
+
+    }
+}
---
+++ Main/Models/bookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Main.Models
+{
+    public class BookModel
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ImageLink { get; set; }
+        public string Year { get; set; }
+        public string Language { get; set; }
+        public string Link { get; set; }
+
+    }
+
+   
+}
